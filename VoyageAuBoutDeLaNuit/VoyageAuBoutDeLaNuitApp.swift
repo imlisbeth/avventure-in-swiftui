7b81059b51e63a36ecc4b477d4e920fda8bbe13d
ليزبيث هالا <llulah@live.com>
2022-08-29 21:45:07 -0300

    Initial Commit

---
+++ VoyageAuBoutDeLaNuit/VoyageAuBoutDeLaNuitApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyageAuBoutDeLaNuitApp.swift
+//  VoyageAuBoutDeLaNuit
+//
+//  Created by ليزبيث هالا on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct VoyageAuBoutDeLaNuitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
